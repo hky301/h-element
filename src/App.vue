41a7e4b391b6f297e7e37c46805fa0c1692d750b
HeKaiYu <hekaiyu_301@163.com>
2023-11-07 16:46:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Button from './components/Button/Button.vue'
+
+const open = () => {
+  // createMessage({ message: 'hello world', duration: 0, showClose: true })
+  // tooltipRef.value?.show()
+}
+const close = () => {
+  // tooltipRef.value?.hide()
+}
+</script>
+
+<template>
+  <main>
+    <Button ref="buttonRef" @click="open">Test Button</Button>
+    <v-button round>Round Button</v-button>
+    <Button plain @click="close">Plain Button</Button>
+    <Button round>Round Button</Button>
+    <Button circle>VK</Button>
+    <Button disabled>Disabled Button</Button><br /><br />
+    <Button type="primary">Primary</Button>
+    <Button type="success">Success</Button>
+    <Button type="info">Info</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="danger">Danger</Button><br /><br />
+    <Button type="primary" plain>Primary</Button>
+    <Button type="success" plain>Success</Button>
+    <Button type="info" plain>Info</Button>
+    <Button type="warning" plain>Warning</Button>
+    <Button type="danger" plain>Danger</Button><br /><br />
+    <Button size="large">Large</Button>
+    <Button size="small">Small</Button><br /><br />
+    <Button size="large" loading>Loading</Button>
+    <Button size="large" icon="arrow-up">Icon</Button><br /><br />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,51 @@
+/// <reference types="vitest" />
+
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      tsconfigPath: './tsconfig.build.json'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'HElement',
+      fileName: 'h-element'
+    },
+    rollupOptions: {
+      external: ['vue', '@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/vue-fontawesome'],
+      output: {
+        exports: 'named',
+        globals: {
+          vue: 'Vue'
+        },
+        assetFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'style.css') {
+            return 'index.css'
+          }
+          return chunkInfo.name as string
+        }
+      }
+    }
+  }
+})
