002d1f763604cf8e31eedb9ccf54365b29907265
HeKaiYu <hekaiyu_301@163.com>
2023-11-16 16:19:23 +0800

    chore: Input

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import Button from '@/components/Button'
 import Collapse, { CollapseItem } from '@/components/Collapse'
-// import Dropdown from '@/components/Dropdown'
+import Dropdown from '@/components/Dropdown'
 // import Form, { FormItem } from '@/components/Form'
 import Icon from '@/components/Icon'
-// import Message, { createMessage, closeAll as closeMessageAll } from '@/components/Message'
-// import Input from '@/components/Input'
+import Message, { createMessage, closeAll as closeMessageAll } from '@/components/Message'
+import Input from '@/components/Input'
 // import Select from '@/components/Select'
 // import Switch from '@/components/Switch'
 import Tooltip from '@/components/Tooltip'
@@ -21,12 +21,12 @@ const components = [
   Button,
   Collapse,
   CollapseItem,
-  // Dropdown,
+  Dropdown,
   // Form,
   // FormItem,
   Icon,
-  // Message,
-  // Input,
+  Message,
+  Input,
   // Select,
   // Switch,
   Tooltip
@@ -43,17 +43,17 @@ export {
   Button,
   Collapse,
   CollapseItem,
-  // Dropdown,
+  Dropdown,
   // Form,
   // FormItem,
   Icon,
-  // Message,
-  // Input,
+  Message,
+  Input,
   // Select,
   // Switch,
   Tooltip,
-  // createMessage,
-  // closeMessageAll
+  createMessage,
+  closeMessageAll
 }
 
 
